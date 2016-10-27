37304f0ebc0294d194b182337e9825a402dff310
schneems <richard.schneeman@gmail.com>
2016-10-27 09:04:06 -0500

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.force_ssl = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
